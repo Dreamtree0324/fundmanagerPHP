88513195995a59ffa7183bb892ee5283e7c739da
Park Minwoo <42574414+HyeN123@users.noreply.github.com>
2019-05-11 16:19:58 +0900

    Add files via upload

--- js/App.js
+++ js/App.js
@@ -1,6 +1,6 @@
 class App {
     constructor() {
-        this.list = document.querySelectorAll("nav > ul > a");
+        this.list = document.querySelectorAll("nav > ul a");
         this.fundList = [];
         this.fundCnt = 1; //현재 펀드 번호
 
@@ -8,7 +8,7 @@ class App {
             x.addEventListener("click", this.changeMenu.bind(this));
         });
 
-         //투자자 리스트
+        //투자자 리스트
         this.invList = [];
 
         this.articleList = document.querySelectorAll("article");
@@ -25,8 +25,6 @@ class App {
 
         document.querySelector("#register button").addEventListener("click", this.registerFund.bind(this));
 
-        this.list[0].click();
-
         this.popup = document.querySelector(".popup");
         document.querySelector("#btnClose").addEventListener("click", this.closePopup.bind(this));
         document.querySelector("#btnInvest").addEventListener("click", this.investFund.bind(this));
@@ -36,7 +34,7 @@ class App {
         this.signCanvas.height = this.signCanvas.clientHeight;
         this.sCtx = this.signCanvas.getContext("2d");
 
-        this.beforePoint= {x:0, y:0};
+        this.beforePoint = { x: 0, y: 0 };
         this.startDraw = false;
         this.signCanvas.addEventListener("mousedown", (e) => {
             this.startDraw = true;
@@ -49,58 +47,114 @@ class App {
         });
 
         this.signCanvas.addEventListener("mousemove", (e) => {
-            if(!this.startDraw) return;
+            if (!this.startDraw) return;
             this.sCtx.beginPath();
             this.sCtx.moveTo(this.beforePoint.x, this.beforePoint.y);
             this.sCtx.lineTo(e.offsetX, e.offsetY);
             this.sCtx.stroke();
             this.beforePoint.x = e.offsetX;
             this.beforePoint.y = e.offsetY;
-            
+
         })
 
         this.toastContainer = document.querySelector("#toastList");
+        this.loadingFundList();
+        this.loadingInvestorList();
+    }
+
+    loadingFundList() {
+        let req = new XMLHttpRequest();
+        req.open("GET", "/fundlist.php");
+        this.fundList = [];
+        req.onreadystatechange = () => {
+            if (req.readyState === XMLHttpRequest.DONE) {
+                if (req.status === 200) {
+                    let json = JSON.parse(req.responseText);
+                    json.data.forEach(x => {
+                        let fund = new Fund(x.id, x.name, x.end_date, x.total, x.current);
+                        this.fundList.push(fund);
+                    });
+                } else {
+                    this.showMsg("전송중 오류 발생");
+                }
+                this.list[0].click();
+            }
+        }
+        req.send();
     }
 
     investFund() {
         let fundNo = document.querySelector("#investNo").value;
-        const fund = this.fundList.find(x => x.number == fundNo);
-        if (!fund) {
-            return;
-        };
-
         let money = document.querySelector("#money").value * 1;
+
         if (money <= 0) {
             this.showMsg("금액을 올바르게 입력하세요");
             return;
         }
-        let result = fund.invest(money);
-        this.showMsg(result.msg);
-        let name = document.querySelector("#name").value;
+
         let signData = this.signCanvas.toDataURL();
 
-        if (result.success) {
-            //투자 성공시
-            this.popup.querySelector("#btnClose").click();
-            
-            let inv = new Investor(fund, name, money, signData);
-            this.invList.push(inv);
+        let req = new XMLHttpRequest();
+        req.open("POST", "/add_fund.php");
 
-            this.list[0].click();
+        req.onreadystatechange = () => {
+            if (req.readyState === XMLHttpRequest.DONE) {
+                if (req.status === 200) {
+                    let json = JSON.parse(req.responseText);
 
-            console.log(inv);
+                    this.showMsg(json.msg);
+                    if (json.success) {
+                        this.popup.querySelector("#btnClose").click();
+                        this.loadingFundList();                        
+                        this.loadingInvestorList();
+                    }
+
+                    console.log(json);
+                } else {
+                    this.showMsg("전송 오류 발생");
+                }
+            }
         }
 
+        let formData = new FormData();
+        formData.append("id", fundNo);
+        formData.append("money", money);
+        formData.append("sign", signData);
+
+        req.send(formData);
     };
 
+    loadingInvestorList(){
+        let req = new XMLHttpRequest();
+        req.open("GET", "/invest_list.php");
+        this.invList = [];
+        req.onreadystatechange = () => {
+            if (req.readyState === XMLHttpRequest.DONE) {
+                if (req.status === 200) {
+                    let json = JSON.parse(req.responseText);
+                    json.data.forEach(x => {
+                        let inv = new Investor({number:x.fid, name:x.fname}, x.uname, x.money, x.sign);
+                        this.invList.push(inv);
+                    });
+                }
+            }
+        }
+        req.send();
+    }
+
     openPopup(fund) {
+
+        if (user == null) {
+            this.showMsg("로그인 후 투자하실 수 있습니다.");
+            return;
+        }
         this.popup.querySelector("#investNo").value = fund.number;
         this.popup.querySelector("#investName").value = fund.name;
-        this.popup.querySelector("#name").value = "";
+        this.popup.querySelector("#name").value = user.name;
         this.popup.querySelector("#money").value = 0;
-        this.sCtx.clearRect(0,0, this.signCanvas.width, this.signCanvas.height);
+        this.sCtx.clearRect(0, 0, this.signCanvas.width, this.signCanvas.height);
         this.popup.classList.add("active");
-        
+
     };
 
     closePopup() {
@@ -147,11 +201,33 @@ class App {
             return;
         }
 
-        let fund = new Fund(no, name, endDate, total);
-        this.fundList.push(fund);
-        this.showMsg("등록되었습니다.");
-        this.fundCnt++;
-        this.list[0].click();
+        let req = new XMLHttpRequest();
+        req.open("POST", "/invest_ok.php");
+
+        req.onreadystatechange = () => {
+            if (req.readyState === XMLHttpRequest.DONE) {
+                if (req.status === 200) {
+                    let json = JSON.parse(req.responseText);
+                    this.showMsg(json.msg);
+                    if (json.success) {
+                        let recvData = json.data;
+                        let fund = new Fund(recvData.id, name, endDate, total);
+                        this.fundList.push(fund);
+                        this.fundCnt++;
+                        this.list[0].click();
+                    }
+                } else {
+                    console.log('문제 발생');
+                }
+            }
+        }
+
+        let formData = new FormData();
+        formData.append("name", name);
+        formData.append("endDate", endDate);
+        formData.append("money", total);
+
+        req.send(formData);
     }
 
     //펀드 리스트 페이지
@@ -169,7 +245,7 @@ class App {
 
     //투자자 목록 페이지
     loadingInvestor() {
-        this.invContainer.innerHTML= "";
+        this.invContainer.innerHTML = "";
         this.invList.forEach(x => {
             this.invContainer.appendChild(x.getTemplate());
         });
@@ -184,8 +260,8 @@ class App {
 
         let closed = false;
 
-        let closeTimer = setTimeout( () => {
-            if(closed) return;
+        let closeTimer = setTimeout(() => {
+            if (closed) return;
             div.style.opacity = 0;
             setTimeout(() => {
                 this.toastContainer.removeChild(div);
@@ -195,7 +271,7 @@ class App {
         div.querySelector(".close").addEventListener("click", () => {
             closed = true;
             div.style.opcity = 0;
-                this.toastContainer.removeChild(div);
+            this.toastContainer.removeChild(div);
         });
 
         this.toastContainer.appendChild(div);
